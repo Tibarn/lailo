06e8e241746754aff08878ff7000c4cac9f0af27
Feliu <ftibarn@gmail.com>
2016-01-17 20:51:26 +0100

    1

---
+++ app/src/main/java/edu/upc/eetac/dsa/beeter/client/BeeterClientException.java
@@ -0,0 +1,8 @@
+package edu.upc.eetac.dsa.beeter.client;
+
+
+public class BeeterClientException extends Exception{
+    public BeeterClientException(String detailMessage) {
+        super(detailMessage);
+    }
+}
---
+++ app/src/main/java/edu/upc/eetac/dsa/beeter/client/entity/Root.java
@@ -0,0 +1,16 @@
+package edu.upc.eetac.dsa.beeter.client.entity;
+
+import java.util.List;
+
+
+public class Root {
+    private List<Link> links;
+
+    public List<Link> getLinks() {
+        return links;
+    }
+
+    public void setLinks(List<Link> links) {
+        this.links = links;
+    }
+}
